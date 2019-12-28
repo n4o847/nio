c6eabacfd46982377c6e7fdea40fbe2c82582e59
n4o847 <22975590+n4o847@users.noreply.github.com>
2019-12-28 20:24:59 +0900

    REPL を改善

--- src/repl.rs
+++ src/repl.rs
@@ -5,19 +5,23 @@ use std::io::{self, BufRead, Write};
 pub fn start() -> io::Result<()> {
     let stdin = io::stdin();
     let mut stdout = io::stdout();
+    write!(stdout, "> ")?;
+    stdout.flush()?;
     for line in stdin.lock().lines() {
         let input = line?;
         let mut l = Lexer::new(&input[..]);
+        writeln!(stdout, "Lexer:")?;
         loop {
             let token = l.next_token();
-            writeln!(stdout, "{:?}", token)?;
+            writeln!(stdout, "  {:?}", token)?;
             match token {
                 Token::EOF | Token::Unexpected(_) => break,
                 _ => (),
             };
         }
         let mut p = Parser::new(&input[..]);
-        writeln!(stdout, "{:?}", p.parse_program())?;
+        writeln!(stdout, "Parser:")?;
+        writeln!(stdout, "  {:?}", p.parse_program())?;
         stdout.flush()?;
     }
     Ok(())
