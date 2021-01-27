42f168f0a1b8d82c73b45db3e86a4d5bfe8a951a
n4o847 <22975590+n4o847@users.noreply.github.com>
2021-01-27 23:06:51 +0900

    Add wasm examples

---
+++ examples/run_add.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+
+const main = async () => {
+  const buffer = fs.readFileSync("./examples/add.wasm");
+  const module = await WebAssembly.compile(buffer);
+  const instance = await WebAssembly.instantiate(module);
+
+  const { add } = instance.exports;
+  console.log(add(1, 2));
+};
+
+main();
