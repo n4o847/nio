002775321fa01f45da56fa9eeee62c29e1ed22a9
n4o847 <22975590+n4o847@users.noreply.github.com>
2021-08-02 17:46:04 +0900

    Recieve subcommand in cli

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
 use nio::repl::Repl;
+use std::env;
+use std::process;
 
 fn main() {
-    Repl::new().start().unwrap();
+    let args: Vec<_> = env::args().collect();
+    match args.get(1).map(String::as_str) {
+        Some(s) => {
+            eprintln!("error: no such subcommand: `{}`", s);
+            process::exit(1);
+        }
+        None => {
+            Repl::new().start().unwrap();
+        }
+    }
 }
