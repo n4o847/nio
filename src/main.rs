222f3179e7ff505527f87ec49d5e7b1472b0b577
n4o847 <22975590+n4o847@users.noreply.github.com>
2021-08-31 23:50:01 +0900

    fix: display help if no subcommands are present

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::{App, Arg};
+use clap::{App, AppSettings, Arg};
 use nio::codegen::CodeGenerator;
 use nio::parser::Parser;
 use std::fs::{self, File};
@@ -9,6 +9,7 @@ type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 fn main() -> Result<()> {
     let matches = App::new("nio")
         .version("0.1.0")
+        .setting(AppSettings::SubcommandRequiredElseHelp)
         .subcommand(App::new("parse"))
         .subcommand(
             App::new("compile")
@@ -22,32 +23,36 @@ fn main() -> Result<()> {
         )
         .get_matches();
 
-    if let Some(_matches) = matches.subcommand_matches("parse") {
-        let mut buffer = String::new();
-        let mut stdin = io::stdin();
-        stdin.read_to_string(&mut buffer)?;
+    match matches.subcommand() {
+        Some(("parse", _)) => {
+            let mut buffer = String::new();
+            let mut stdin = io::stdin();
+            stdin.read_to_string(&mut buffer)?;
 
-        let result = Parser::parse(buffer.as_str());
-        println!("{:?}", result);
-    }
+            let result = Parser::parse(buffer.as_str());
+            println!("{:?}", result);
+        }
 
-    if let Some(matches) = matches.subcommand_matches("compile") {
-        let source = matches.value_of("INPUT").unwrap();
-        let target = &match matches.value_of("output") {
-            Some(target) => target.to_string(),
-            None => source.strip_suffix(".nio").unwrap_or(source).to_string() + ".wasm",
-        }[..];
+        Some(("compile", matches)) => {
+            let source = matches.value_of("INPUT").unwrap();
+            let target = &match matches.value_of("output") {
+                Some(target) => target.to_string(),
+                None => source.strip_suffix(".nio").unwrap_or(source).to_string() + ".wasm",
+            }[..];
 
-        eprintln!("Compile {}", fs::canonicalize(source)?.display());
-        let input = fs::read_to_string(source)?;
+            eprintln!("Compile {}", fs::canonicalize(source)?.display());
+            let input = fs::read_to_string(source)?;
 
-        let program = Parser::parse(&input)?;
+            let program = Parser::parse(&input)?;
 
-        let module = CodeGenerator::generate(&program)?;
+            let module = CodeGenerator::generate(&program)?;
 
-        eprintln!("Emit {}", fs::canonicalize(target)?.display());
-        let mut output = File::create(target)?;
-        nio::wasm::binary::emit(&mut output, &module)?;
+            eprintln!("Emit {}", fs::canonicalize(target)?.display());
+            let mut output = File::create(target)?;
+            nio::wasm::binary::emit(&mut output, &module)?;
+        }
+
+        _ => {}
     }
 
     Ok(())
