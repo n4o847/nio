47cee70d3863f1548dad8139579cdefe5ad1a14a
n4o847 <22975590+n4o847@users.noreply.github.com>
2021-09-17 14:07:13 +0900

    refactor(parser): rewrite tests

--- crates/parser/src/lib.rs
+++ crates/parser/src/lib.rs
@@ -19,65 +19,21 @@ pub fn parse(input: &str) -> Result<ast::Program, ParseError<Location, lexer::To
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_parse() {
-        use ast::BinOp::*;
-        use ast::Expr::*;
-        use ast::Program;
-        use ast::Stmt::*;
-
-        assert_eq!(
-            parse("123"),
-            Ok(Program {
-                statements: vec![Expr(IntLit("123".to_string()))]
-            })
-        );
-
-        assert_eq!(
-            parse("1 + 2 * 3 * 4"),
-            Ok(Program {
-                statements: vec![Expr(BinOp {
-                    op: Add,
-                    left: Box::new(IntLit("1".to_string())),
-                    right: Box::new(BinOp {
-                        op: Mul,
-                        left: Box::new(BinOp {
-                            op: Mul,
-                            left: Box::new(IntLit("2".to_string())),
-                            right: Box::new(IntLit("3".to_string()))
-                        }),
-                        right: Box::new(IntLit("4".to_string()))
-                    })
-                })]
-            })
-        );
-
-        assert_eq!(
-            parse("|x| x + 1"),
-            Ok(Program {
-                statements: vec![Expr(Lambda {
-                    params: vec!["x".to_string()],
-                    body: Box::new(BinOp {
-                        op: Add,
-                        left: Box::new(Ident("x".to_string())),
-                        right: Box::new(IntLit("1".to_string()))
-                    })
-                })]
-            })
-        );
-
-        assert_eq!(
-            parse("a + b(x, y)"),
-            Ok(Program {
-                statements: vec![Expr(BinOp {
-                    op: Add,
-                    left: Box::new(Ident("a".to_string())),
-                    right: Box::new(Call {
-                        callee: Box::new(Ident("b".to_string())),
-                        args: vec![Ident("x".to_string()), Ident("y".to_string()),]
-                    })
-                })]
-            })
-        );
+    macro_rules! test_parse {
+        ($test_name:ident, $name:expr) => {
+            #[test]
+            fn $test_name() {
+                let input = include_str!(concat!("../tests/", $name, ".in"));
+                let output = include_str!(concat!("../tests/", $name, ".out"));
+                let result = &format!("{:#?}", parse(input));
+                assert_eq!(result, output, "expected: {}", result);
+            }
+        };
     }
+
+    test_parse!(test_parse_empty, "empty");
+    test_parse!(test_parse_int_lit, "int_lit");
+    test_parse!(test_parse_expr, "expr");
+    test_parse!(test_parse_lambda, "lambda");
+    test_parse!(test_parse_call, "call");
 }
