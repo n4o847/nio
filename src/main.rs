3658bdbd6cb5208547cc33da62c876e3cd9c9338
n4o847 <22975590+n4o847@users.noreply.github.com>
2021-09-17 16:05:20 +0900

    refactor: use CARGO_PKG_VERSION

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 fn main() -> Result<()> {
     let matches = App::new("nio")
-        .version("0.1.0")
+        .version(env!("CARGO_PKG_VERSION"))
         .setting(AppSettings::SubcommandRequiredElseHelp)
         .subcommand(App::new("parse"))
         .subcommand(
